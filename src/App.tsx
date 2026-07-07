a77162e45cb555d1fc56734ea845200b3d50aea4
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-07-07 22:23:36 +0200

    -------

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services"
 
 function App(){
     return (
         <>
             <Navbar />
             <Hero />
+            <Services />
         </>
     );
 }
