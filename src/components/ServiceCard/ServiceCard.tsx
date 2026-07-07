59d7a0e8fe66a160bc904845e4a5ddd0a2959833
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-07-07 20:02:40 +0200

    Add ServiceCard component

---
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,23 @@
+import "./ServiceCard.css";
+
+
+type ServiceCardProps{
+    icon: string;
+    title: string;
+    description: string;
+}
+
+function ServiceCard({ icon , title , description } : ServiceCardProps){
+    return(
+        <div className="service-card">
+
+            <img src={icon} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+
+        </div>
+
+    );
+}
+
+export default ServiceCard;
